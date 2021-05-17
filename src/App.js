9591dc173eb91951c64e81aa8dbf35c4958dab06
AbhisheK Rathore <rathore.abhishek87@gmail.com>
2021-05-17 16:42:45 +0530

    react-portal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import {Modal, ModalContent} from './modal';
 
 function App() {
   return (
     <div className="App">
       <h1>App</h1>
-      <Modal>
-        <ModalContent></ModalContent>
-      </Modal>
+   
     </div>
   );
 }
